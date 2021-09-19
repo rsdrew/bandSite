782e5b877dc5b46fec691c095963e3c00e18e951
Ryan Drew <rsdrew@umich.edu>
2021-09-19 16:47:07 -0400

    error

---
+++ app.js
@@ -0,0 +1,48 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+var indexRouter = require('./routes/index');
+var aboutRouter = require('./routes/about');
+var musicRouter = require('./routes/music');
+var showsRouter = require('./routes/shows');
+var contactRouter = require('./routes/contact');
+app.use('/', indexRouter);
+app.use('/about', aboutRouter);
+app.use('/music', musicRouter);
+app.use('/shows', showsRouter);
+app.use('/contact', contactRouter);
+
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+// app.use(function(err, req, res, next) {
+//   // set locals, only providing error in development
+//   res.locals.message = err.message;
+//   res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+//   // render the error page
+//   res.status(err.status || 500);
+//   res.render('error');
+// });
+
+module.exports = app;
